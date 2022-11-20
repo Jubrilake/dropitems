27b8fb9f1a3b2c3a614deb00627d077ffd6e7b29
Adebayo <jubrilake@gmail.com>
2022-11-20 04:28:39 +0100

    first commit

---
+++ setup/src/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img:
+      'https://priceanywhere.com/wp-content/uploads/thumbs_dir/galaxy-s7-edge-32gb-att-phones-sm-g935azkaatt-samsung-us-1-ns8etwwahhyigw9ps0o2yp92e76c1c9b9mh4tgfuvc.jpg',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img:
+      'https://lh3.googleusercontent.com/VUYKrmiuvsE1dn5L-58clmUX2pvOio6Cu7YZoFfWUaE8NLEV5mWy_qzMmKTor-26hR4vK5-L5Ro7h9GmnDRLdX0BzOXPG2ErYw=rw-e365-w525',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img:
+      'https://www.devicesfaq.com/img/xiaomi-redmi-note-2-prime.jpg',
+    amount: 1,
+  },
+];
